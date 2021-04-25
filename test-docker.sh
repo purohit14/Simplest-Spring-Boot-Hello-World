216be98b465999dcb9f668426bc0b6a20d60b207
purohit14 <ashutoshpurohit14@gmail.com>
2021-04-25 12:11:06 +0530

    added a env file regarding local var

--- test-docker.sh
+++ test-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --name good-image-${BUILD_NUMBER} ${ECR_PRIVATE_REPO_LINK}/${ECR_REPO_NAME}:JAVA-APP-v${BUILD_NUMBER}
+docker run -d --name good-image-${BUILD_NUMBER}-${region} ${ECR_PRIVATE_REPO_LINK}/${ECR_REPO_NAME}:JAVA-APP-v${BUILD_NUMBER}
